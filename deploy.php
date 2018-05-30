a356ef9459f5347c46fb48bdf1eefa21869261be
Vladimir <rezork@gmail.com>
2018-05-30 19:31:32 +0500

    fix deploy script

--- deploy.php
+++ deploy.php
@@ -8,6 +8,8 @@ if ($_GET['key'] === 'bxn5XNWNEoHetCtJgjgUzJ2ocLYeWeeRpKOyw') {
     exec('git reset --hard HEAD', $output);
     print_r($output);
 
+    $output = '';
+
     exec('git pull origin master', $output);
     print_r($output);
 
